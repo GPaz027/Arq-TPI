5a2982f2264d96abf5d056877a564388538b2e30
Gonzalo Paz <pazgonzalo2014@gmail.com>
2023-04-02 22:22:24 -0300

    Configuration files completed and working

---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,346 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.9"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+
+# VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = var.vpc_cidr
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name                                           = "${var.project}-vpc",
+    "kubernetes.io/cluster/${var.project}-cluster" = "shared"
+  }
+}
+
+# Public Subnets
+resource "aws_subnet" "public" {
+  count = var.availability_zones_count
+
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                                           = "${var.project}-public-sg"
+    "kubernetes.io/cluster/${var.project}-cluster" = "shared"
+    "kubernetes.io/role/elb"                       = 1
+  }
+
+  map_public_ip_on_launch = true
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count = var.availability_zones_count
+
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, count.index + var.availability_zones_count)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                                           = "${var.project}-private-sg"
+    "kubernetes.io/cluster/${var.project}-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"              = 1
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    "Name" = "${var.project}-igw"
+  }
+
+  depends_on = [aws_vpc.eks_vpc]
+}
+
+# Route Table(s)
+# Route the public subnet traffic through the IGW
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.project}-Default-rt"
+  }
+}
+
+# Route table and subnet associations
+resource "aws_route_table_association" "internet_access" {
+  count = var.availability_zones_count
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.main.id
+}
+
+# NAT Elastic IP
+resource "aws_eip" "main" {
+  vpc = true
+
+  tags = {
+    Name = "${var.project}-ngw-ip"
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.main.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.project}-ngw"
+  }
+}
+
+# Add route to route table
+resource "aws_route" "main" {
+  route_table_id         = aws_vpc.eks_vpc.default_route_table_id
+  nat_gateway_id         = aws_nat_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# Security group for public subnet
+resource "aws_security_group" "public_sg" {
+  name   = "${var.project}-Public-sg"
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "${var.project}-Public-sg"
+  }
+}
+
+# Security group traffic rules
+resource "aws_security_group_rule" "sg_ingress_public_443" {
+  security_group_id = aws_security_group.public_sg.id
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "sg_ingress_public_80" {
+  security_group_id = aws_security_group.public_sg.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "sg_egress_public" {
+  security_group_id = aws_security_group.public_sg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# Security group for data plane
+resource "aws_security_group" "data_plane_sg" {
+  name   = "${var.project}-Worker-sg"
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "${var.project}-Worker-sg"
+  }
+}
+
+# Security group traffic rules
+resource "aws_security_group_rule" "nodes" {
+  description       = "Allow nodes to communicate with each other"
+  security_group_id = aws_security_group.data_plane_sg.id
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  cidr_blocks       = flatten([cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 0), cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 1), cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 2), cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 3)])
+}
+
+resource "aws_security_group_rule" "nodes_inbound" {
+  description       = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+  security_group_id = aws_security_group.data_plane_sg.id
+  type              = "ingress"
+  from_port         = 1025
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = flatten([cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 2), cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 3)])
+}
+
+resource "aws_security_group_rule" "node_outbound" {
+  security_group_id = aws_security_group.data_plane_sg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# Security group for control plane
+resource "aws_security_group" "control_plane_sg" {
+  name   = "${var.project}-ControlPlane-sg"
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "${var.project}-ControlPlane-sg"
+  }
+}
+
+# Security group traffic rules
+resource "aws_security_group_rule" "control_plane_inbound" {
+  security_group_id = aws_security_group.control_plane_sg.id
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = flatten([cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 0), cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 1), cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 2), cidrsubnet(var.vpc_cidr, var.subnet_cidr_bits, 3)])
+}
+
+resource "aws_security_group_rule" "control_plane_outbound" {
+  security_group_id = aws_security_group.control_plane_sg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "this" {
+  name     = "${var.project}-cluster"
+  role_arn = "arn:aws:iam::285542564450:role/LabRole"
+  version  = "1.21"
+
+  vpc_config {
+    subnet_ids              = flatten([aws_subnet.public[*].id, aws_subnet.private[*].id])
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    var.tags
+  )
+}
+
+# EKS Cluster Security Group
+resource "aws_security_group" "eks_cluster" {
+  name        = "${var.project}-cluster-sg"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "${var.project}-cluster-sg"
+  }
+}
+
+resource "aws_security_group_rule" "cluster_inbound" {
+  description              = "Allow worker nodes to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "cluster_outbound" {
+  description              = "Allow cluster API Server to communicate with the worker nodes"
+  from_port                = 1024
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 65535
+  type                     = "egress"
+}
+
+
+# EKS Node Groups
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = var.project
+  node_role_arn   = "arn:aws:iam::285542564450:role/LabRole"
+  subnet_ids      = aws_subnet.private[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  ami_type       = "AL2_x86_64" # AL2_x86_64, AL2_x86_64_GPU, AL2_ARM_64, CUSTOM
+  capacity_type  = "ON_DEMAND"  # ON_DEMAND, SPOT
+  disk_size      = 20
+  instance_types = ["t2.medium"]
+
+  tags = merge(
+    var.tags
+  )
+}
+
+
+# EKS Node Security Group
+resource "aws_security_group" "eks_nodes" {
+  name        = "${var.project}-node-sg"
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = aws_vpc.eks_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name                                           = "${var.project}-node-sg"
+    "kubernetes.io/cluster/${var.project}-cluster" = "owned"
+  }
+}
+
+resource "aws_security_group_rule" "nodes_internal" {
+  description              = "Allow nodes to communicate with each other"
+  from_port                = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "nodes_cluster_inbound" {
+  description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+  from_port                = 1025
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.eks_cluster.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
